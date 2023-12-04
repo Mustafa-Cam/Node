ec0b998a67c79246e7197e555082d0edc4937ce5
muustafa çam <mustafa.rc1905@gmail.com>
2023-12-04 20:12:04 +0300

    add js example

--- post.js
+++ post.js
@@ -40,6 +40,35 @@ let postlist = [
   
   // async fonksiyonu çağırma
      addPost({ name: "aslan akbey", post: "c" });
+
+
+     //! bu örnek daha güzel online js compiler de deneyebilirsin => 
+
+     // Asenkron bir fonksiyon simüle edelim
+// const fetchData = () => {
+//     return new Promise((resolve) => {
+//       // Simüle edilmiş asenkron bir işlem
+//       setTimeout(() => {
+//         resolve("Veri başarıyla alındı!");
+//       }, 2000);
+//     });
+//   };
+  
+//   // async fonksiyonu tanımlayalım
+//   const fetchDataAsync = async () => {
+//     console.log("Veri alımı başlıyor...");
+  
+//     // await ifadesi, asenkron işlemin tamamlanmasını bekler
+//     const result = await fetchData();
+  
+//     // Sonuç elde edildikten sonra işleme devam eder
+//     console.log(result);
+//     console.log("Veri alımı tamamlandı!");
+//   };
+  
+//   // async fonksiyonu çağıralım
+//   fetchDataAsync();
+  
   
 
   
